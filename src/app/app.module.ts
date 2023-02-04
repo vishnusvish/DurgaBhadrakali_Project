304e053d9812577cd35cbbfec8cccb0bc25383c5
vishnusvish <vishnusvish96@gmail.com>
2023-02-04 16:28:45 +0530

    message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { Section1Component } from './shared/section1/section1.component';
+import { FestivalComponent } from './pages/festival/festival.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    MenuComponent,
+    AboutComponent,
+    ContactComponent,
+    HeaderComponent,
+    Section1Component,
+    FestivalComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/festival/festival.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FestivalComponent } from './festival.component';
+
+describe('FestivalComponent', () => {
+  let component: FestivalComponent;
+  let fixture: ComponentFixture<FestivalComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FestivalComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(FestivalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
